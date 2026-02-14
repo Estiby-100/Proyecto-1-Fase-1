667abb74c3f359407e6a2541f119be8c4733fe40
Hola2212 <juanpaflores06@gmail.com>
2026-02-13 20:29:36 -0600

    Completed Main Class code

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Parser parser = new Parser();
+        Collection<String> stack = new PilaArrayList<>();
+        Interpreter interpreter = new Interpreter(stack);
+        FileReader dataObtainer = new FileReader();
+        String script = dataObtainer.ReadFile("target/Script_commands.txt");
+        List<Token> tokens = parser.parse(script);
+        boolean result = interpreter.execute(tokens);
+        System.out.println("Resultado: " + result);
+    }
+}
